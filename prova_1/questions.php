8bf9e27af72549b804db0a5d555672514d66cb03
Fabio Lopes <nunes-lopes@hotmail.com>
2024-09-16 21:23:43 -0300

    30 questões

--- prova_1/questions.php
+++ prova_1/questions.php
@@ -9,7 +9,7 @@ return [
             'ter ampla defesa;',
             'desfrutar do trânsito em condições seguras.'
         ],
-        'correct' => 2 // Índice da resposta correta
+        'correct' => 2 // Índice da resposta correta QUESTÃO 1
     ],
     [
         'question' => "Para ser aprovado no exame teórico, o candidato ou condutor, deve acertar, no mínimo,",
@@ -20,7 +20,7 @@ return [
             '80% das questões propostas;',
             '70% das questões propostas.'
         ],
-        'correct' => 4 // Índice da resposta correta
+        'correct' => 4 // Índice da resposta correta QUESTÃO 2
     ],
     [
         'question' => "Assinalar a alternativa correta. ",
@@ -31,10 +31,11 @@ return [
             'O tráfego é de mão dupla, quando uma pista de rolamento é dividida por uma linha amarela ao longo do seu eixo.',
             'Silvos e gestos do agente de trânsito são os principais componentes da sinalização temporária.'
         ],
-        'correct' => 3 // Índice da resposta correta
+        'correct' => 3 // Índice da resposta correta QUESTÃO 3
     ],
     [
-        'question' => "Ficou provado que o acidente de trânsito aconteceu na via devido à falta de sinalização. De quem é a responsabilidade pelo acidente?",
+        'question' => "Ficou provado que o acidente de trânsito aconteceu na via devido à falta de sinalização.
+         De quem é a responsabilidade pelo acidente?",
         'answers' => [
             'Do DETRAN.',
             'Do órgão com jurisdição sobre a via.',
@@ -42,10 +43,12 @@ return [
             'Da Circunscrição Regional de Trânsito - CIRETRAN.',
             'Sempre do Departamento de Infraestrutura de Transportes - DNIT'
         ],
-        'correct' => 1 // Índice da resposta correta
+        'correct' => 1 // Índice da resposta correta QUESTÃO 4
     ],
     [
-        'question' => "05-Assinalar a alternativa correta entre o tipo de via e a velocidade MÍNIMA que um condutor pode desenvolver ao trafegar por um trecho não abrangido pela sinalização regulamentadora de velocidade, conduzindo o veiculo indicado:",
+        'question' => "Assinalar a alternativa correta entre o tipo de via e a velocidade MÍNIMA
+         que um condutor pode desenvolver ao trafegar por um trecho não abrangido pela sinalização regulamentadora de velocidade,
+          conduzindo o veiculo indicado:",
         'answers' => [
             'ônibus - estrada - 50km/h;',
             'automóvel - rodovia de pista simples - 60km/h;',
@@ -53,32 +56,298 @@ return [
             'motocicleta - via de trânsito rápido - 45km/h;',
             'caminhão - via arterial - 35km/h.'
         ],
-        'correct' => 2 // Índice da resposta correta
+        'correct' => 2 // Índice da resposta correta QUESTÃO 5
+    ],
+    [
+        'question' => "Dois veículos circulam por uma avenida,
+         sendo esta uma via arterial, por um trecho não abrangido pela sinalização regulamentadora de velocidade.
+          O veículo A circula a 90km/h, e o veiculo B a 70 km/h. Quem está desrespeitando a velocidade máxima permitida?",
+        'answers' => [
+            'Somente o veículo A.',
+            'Somente o veículo B.',
+            'abcSomente o veiculo A, se for um caminhão.',
+            'Os dois veículos.',
+            'Nenhum dos dois.'
+        ],
+        'correct' => 3 // Índice da resposta correta QUESTÃO 6
+    ],
+    [
+        'question' => "Com base na legislação, considerando também a Lei 14.071/20,
+         em relação ao transporte de crianças, é correto afirmar que",
+        'answers' => [
+            'crianças de até dois anos de idade devem viajar no banco traseiro, no colo de um adulto, usando ambos o mesmo o cinto de segurança;',
+            'crianças com mais de um metro de altura podem viajar no banco dianteiro, usando cinto de segurança;',
+            'crianças entre quatro e sete anos podem viajar no banco dianteiro, utilizando cinto de segurança:',
+            'crianças com mais de sete anos e meio até dez anos, e com altura inferior a 1,45 m,
+             devem ser transportadas no banco traseiro, utilizando cinto de segurança:',
+            'crianças com dois anos de idade devem viajar no banco traseiro utilizando bebê conforto.'
+        ],
+        'correct' => 3 // Índice da resposta correta QUESTÃO 7
+    ],
+    [
+        'question' => "08-Segundo o Código, é infração de natureza gravíssima conduzir o veículo",
+        'answers' => [
+            'utilizando-se de fones de ouvido conectados a aparelhagem sonora ou de telefone celular;',
+            'conversando animadamente com os passageiros',
+            'utilizando uma das mãos para segurar ou manusear o telefone celular,',
+            'sem portar o documento de habilitação;',
+            'sem equipamento obrigatório ou estando este sem condições de funcionamento.'
+        ],
+        'correct' => 3 // Índice da resposta correta QUESTÃO 8
+    ],
+    [
+        'question' => "É penalizado com multa x 5 o condutor que",
+        'answers' => [
+            'ultrapassa outro veiculo pela contramão em trechos demarcados por linha amarela continua;',
+            'dirige estando com o direito de dirigir suspenso:',
+            'disputa corrida na via pública;',
+            'participa de eventos na via sem permissão da autoridade;',
+            'exibe manobra perigosa mediante arrancada brusca.'
+        ],
+        'correct' => 0 // Índice da resposta correta QUESTÃO 9
+    ],
+    [
+        'question' => "O condutor foi autuado por conduzir veículo com o direito de dirigir suspenso. Nesse caso, será penalizado com",
+        'answers' => [
+            'multa agravada com fator multiplicador 10;',
+            'a cassação do direito de dirigir;',
+            'nova suspensão do direito de dirigir, tornando-se reincidente;',
+            'realização de curso de reciclagem com aprovação em prova teórica;',
+            'remoção do veículo e recolhimento da CNH.'
+        ],
+        'correct' => 1 // Índice da resposta correta QUESTÃO 10
+    ],
+    [
+        'question' => "São crimes de trânsito ",
+        'answers' => [
+            'avançar o sinal vermelho/fechar cruzamentos;',
+            'dirigir sem habilitação/ estacionar em local proibido;',
+            'participar de disputas não autorizadas com dano potencial à incolumidade /omitir socorro;',
+            'transitar na contramão/transitar no acostamento;',
+            'dirigir em excesso de velocidade/ dirigir sob efeito de álcool.'
+        ],
+        'correct' => 2 // Índice da resposta correta QUESTÃO 11
+    ],
+    [
+        'question' => "Assinalar a alternativa INCORRETA sobre reciclagem, considerando também as alterações trazidas pela Lei 14.071/20.",
+        'answers' => [
+            'O condutor que exerce atividade remunerada ao veículo poderá participar de curso preventivo de reciclagem (art. 161, § 5º, Lei 14.071/20).',
+            'Será aplicada a penalidade de suspensão do direito de dirigir ao condutor que, no período de 12 meses,
+             atingir a contagem de 20 pontos, caso constem duas ou mais infrações gravíssimas na pontuação (art. 261, 1, a, Lei 14.071/20).',
+            'O infrator (condutor ou proprietário) autuado pode optar por ser notificado por meio eletrônico também na reciclagem.',
+            'O condutor que tem seu direito de dirigir suspenso por infrações especificas
+             que preveem essa penalidade deve ficar obrigatoriamente sem dirigir pelo período de um ano. ',
+            'Na suspensão do direito de dirigir, após cumpridas as penalidades e decorrido o prazo,
+             o condutor passa a usufruir novamente do seu direito de dirigir.'
+        ],
+        'correct' => 3 // Índice da resposta correta QUESTÃO 12
+    ],
+    [
+        'question' => "Ao efetuar curvas em rodovias, o condutor deve ",
+        'answers' => [
+            'reduzir a velocidade antes de entrar na curva e depois acelerar gradativamente;',
+            'acelerar antes da curva para, assim, aumentar o poder de tração nas rodas;',
+            'frear durante a curva para evitar a ação da força centrifuga; ',
+            'mudar de marcha, passando para uma de menor rotação, aumentando a tração das rodas;',
+            'acelerar antes de entrar na curva e frear durante a curva.'
+        ],
+        'correct' => 0 // Índice da resposta correta QUESTÃO 13
+    ],
+    [
+        'question' => "São elementos importantes na prática da Direção Defensiva ",
+        'answers' => [
+            'Conhecimento, atenção, previsão, habilidade, ação.',
+            'Atenção, previsão, imprudência, habilidade, ação.',
+            'Conhecimento, previsibilidade, imperícia, habilidade, atenção.',
+            'Previsão, atenção, ação, habilidade, imperícia.',
+            'Habilidade, imprudência, imperícia, negligência, ação.'
+        ],
+        'correct' => 0 // Índice da resposta correta QUESTÃO 14
+    ],
+    [
+        'question' => "Para evitar uma possível colisão com o veículo que segue à frente, a atitude mais importante a tomar é",
+        'answers' => [
+            'manter velocidade constante;',
+            'sempre diminuir a velocidade;',
+            'conservar-se à direita da pista;',
+            'evitar ultrapassagens pela esquerda;',
+            'observar a regra dos dois segundos.'
+        ],
+        'correct' => 4 // Índice da resposta correta QUESTÃO 15
+    ],
+    [
+        'question' => "É um tipo de atenção que o condutor utiliza para perceber também o que se passa ao redor,
+         evitando ficar focado num só ponto da direção. Trata-se da atenção",
+        'answers' => [
+            'concentrada;',
+            'motora;',
+            'dispersa;',
+            'sustentada;',
+            'difusa.'
+        ],
+        'correct' => 4 // Índice da resposta correta QUESTÃO 16
+    ],
+    [
+            'question' => "São fatores físicos diretamente ligados ao condutor que determinam como ele se comporta no trânsito",
+            'answers' => [
+                'fadiga, atenção, audição e visão;',
+                'atenção, tempo, audição e chuva;',
+                'tempo, atenção, neblina e audição;',
+                'veiculo, tempo, atenção e fadiga:',
+                'atenção, audição, veículo e via.'
+            ],
+            'correct' => 0 // Índice da resposta correta QUESTÃO 17
+    ],
+    [
+        'question' => "Ao se aproximar do semáforo, o sinal muda para o amarelo.
+         Você percebe que atrás vem um veiculo muito próximo ao seu. Nessa situação você deve ",
+        'answers' => [
+            'dar sinal de braço, frear intermitentemente para acionar a luz do freio alertando o condutor e tentar parar antes da faixa de segurança;',
+            'acelerar para cruzar, mesmo que corra o risco de ter de passar no sinal vermelho;',
+            'dar sinal de braço e frear bruscamente, para obrigar o outro também a parar,',
+            'frear e mudar de faixa para dar passagem ao outro condutor, ',
+            'tentar mudar de faixa para se livrar de uma possível colisão traseira.'
+        ],
+        'correct' => 0 // Índice da resposta correta QUESTÃO 18
+    ],
+    [
+        'question' => "Estacionar sobre a calçada é",
+        'answers' => [
+            'permitido quando os passageiros vão embarcar ou desembarcar do veículo;',
+            'permitido apenas para veículos de duas ou três rodas: ',
+            'uma infração de trânsito e um desrespeito aos direitos dos pedestres',
+            'permitido nas proximidades de restaurantes e bares noturnos;',
+            'permitido se o veículo estiver com apenas duas rodas sobre a calçada.'
+        ],
+        'correct' => 2 // Índice da resposta correta QUESTÃO 19
+    ],
+    [
+        'question' => "São componentes que podem ou não fazer parte de um capacete certificado pelo INMETRO, EXCETO (MENOS)",
+        'answers' => [
+            'sistema de retenção:',
+            'pala;',
+            'sobreviseira;',
+            'queixeira removível;',
+            'máscara.'
+        ],
+        'correct' => 0 // Índice da resposta correta QUESTÃO 20
+    ],
+    [
+        'question' => "É proibido colocar película",
+        'answers' => [
+            'na viseira do capacete do condutor somente;',
+            'na viseira do capacete do condutor e nos óculos de proteção do condutor, somente;',
+            'na viseira do capacete do passageiro e nos óculos de proteção do passageiro somente;',
+            'na viseira do capacete do passageiro somente;',
+            'na viseira dos capacetes do condutor e do passageiro e nos óculos de proteção do condutor e do passageiro.'
+        ],
+        'correct' => 1 // Índice da resposta correta QUESTÃO 21
+    ],
+    [
+        'question' => "Um condutor de motocicleta transita por uma via de mão dupla,
+         numa situação de chuva muito intensa, a ponto de formar poças d'água sobre a pista. Nesse caso, o piloto deve",
+        'answers' => [
+            'andar na linha divisória da pista para desviar das poças d&#39;&aacute;gua',
+            'aumentar a velocidade para aumentar a aderência dos pneus ao solo;',
+            'diminuir a velocidade, freando a moto constantemente para desviar das poças d&#39;&aacute;gua;',
+            'diminuir a velocidade e frear a moto cada vez que tiver de passar por cima de uma poça d&#39;&aacute;gua;',
+            'diminuir a velocidade e manter-se à direita da pista, transitando com cuidado.'
+        ],
+        'correct' => 4 // Índice da resposta correta QUESTÃO 22
+    ],
+    [
+        'question' => "O que NÃO colabora para diminuir o consumo de combustível?",
+        'answers' => [
+            'Ao trocar as marchas, diminuir a aceleração.',
+            'Transitar com o veiculo com excesso de peso.',
+            'Trocar periodicamente o filtro de combustível.',
+            'Fazer inspeção periódica no veiculo.',
+            'Abastecer o veiculo com combustível de boa qualidade.'
+        ],
+        'correct' => 1 // Índice da resposta correta QUESTÃO 23
+    ],
+    [
+        'question' => "Qual é a maneira correta de o piloto e o passageiro transitar com o capacete? ",
+        'answers' => [
+            'Colocado na cabeça e firmemente afixado por debaixo do maxilar inferior através da cinta jugular e engates.',
+            'Afixado na cabeça, da maneira que mais lhe convier.',
+            'Transportado no braço ou afixado nos suportes específicos existentes na motocicleta. ',
+            'O piloto deve usar o capacete com viseira abaixada, o que não é obrigado ao passageiro.',
+            'O uso da viseira ou óculos de proteção é obrigatório apenas ao piloto.'
+        ],
+        'correct' => 0 // Índice da resposta correta QUESTÃO 24
+    ],
+    [
+        'question' => "Levando em consideração também as alterações trazidas pela Lei 14.071/20,
+         assinale a alternativa que contém a correspondência correta entre a utilização ou não do equipamento e a natureza da infração. O piloto transita",
+        'answers' => [
+            'sem utilizar capacete de segurança - infração grave;',
+            'sem utilizar vestuário de acordo com a regulamentação do CONTRAN- infração média;',
+            'utilizando capacete com a viseira levantada (sem utilizar viseira) - infração média;',
+            'utilizando capacete sem óculos de proteção – infração grave;',
+            'utilizando capacete sem viseira ou óculos de proteção - infração gravíssima.'
+        ],
+        'correct' => 2 // Índice da resposta correta QUESTÃO 25
+    ],
+    [
+        'question' => "Componente importante do veículo para a redução dos níveis de poluição do ar ou atmosférica. Trata-se do(a) ",
+        'answers' => [
+            'silenciador;',
+            'catalisador;',
+            'cano de escapamento;',
+            'válvula de admissão;',
+            'válvula de escape.'
+        ],
+        'correct' => 1 // Índice da resposta correta QUESTÃO 26
+    ],
+    [
+        'question' => "O veículo começa a falhar. O condutor estaciona no acostamento, sinalizando adequadamente.
+         É constatado um problema na bateria, vindo o liquido a vazar sobre o braço.
+          Nesse caso, seguindo o que recomendam os fabricantes de baterias, o que o condutor deve fazer?
+",
+        'answers' => [
+            'Lavar com água boricada.',
+            'Lavar com água oxigenada.',
+            'Lavar com água corrente e depois procurar socorro médico.',
+            'Não fazer nada e ir ao socorro médico.',
+            'Colocar gelo porque abafa o calor da queimadura.'
+        ],
+        'correct' => 2 // Índice da resposta correta QUESTÃO 27
+    ],
+    [
+        'question' => "Ao realizar uma avaliação primária numa vítima de acidente de trânsito, o PRIMEIRO item que deve ser avaliado é",
+        'answers' => [
+            'circulação e proteção da vitima;',
+            'vias aéreas e coluna cervical;',
+            'fraturas e hemorragias;',
+            'nível de consciência e respiração;',
+            'respiração e temperatura corporal.'
+        ],
+        'correct' => 0 // Índice da resposta correta QUESTÃO 28
     ],
     [
         'question' => "Veja a imagem abaixo:<br><img src='placa_mercosul.png' alt='Placa Padrão mercosul' style='max-width: 100%; height: auto;'>",
         'answers' => [
-            'abc',
-            'abc',
+            'ABC',
+            'ABC',
             'abc',
             'abc',
             'abc'
         ],
-        'correct' => 2 // Índice da resposta correta
+        'correct' => 0 // Índice da resposta correta QUESTÃO 29
     ],
     [
         'question' => "Veja a imagem abaixo:<br><img src='placa_mercosul.png' alt='Placa Padrão mercosul' style='max-width: 100%; height: auto;'>",
         'answers' => [
-            'abc',
-            'abc',
+            'ABC',
+            'ABC',
             'abc',
             'abc',
             'abc'
         ],
-        'correct' => 2 // Índice da resposta correta
+        'correct' => 0 // Índice da resposta correta QUESTÃO 30
     ],
-  
-  
+    
     // Adicione mais perguntas aqui
 ];
 ?>
